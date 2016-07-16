546efe88ccd4b8e091723c5332d7430f6c31eaf7
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-07-16 14:41:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,24 +8,38 @@ if(typeof jQuery === "undefined"){
 
 engine.on('msg', function(data) {
 	message = data.message;
-	if (message.indexOf("!cointrust") >= 0) {
-		tokens = message.split(" ", 2);
-		console.log(tokens);
-		console.log(tokens[1]);
-		$.ajax({
-			dataType: "json",
-			url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + tokens[1],
-			data1: data,
-			success: function (data1){
-				console.log(data1);
-				array = data1[0];
-				if(typeof array !== "undefined"){
-					engine.chat("Username: "+ array.uname +" | Suspicion Level: " + array.suspicion + " | Read the full profile at: " + array.link);
-				}else{
-					
-					engine.chat("User not found!");
-				}
+	if(message.indexOf("!cointrust") == 0) {
+		tokens = message.split(" ");
+		if(tokens[1] == "help"){
+			engine.chat("View my Wiki: https://github.com/FinlayDaG33k/TrustyBot/wiki/Commands/");
+		}else if(tokens[1] == "donate"){
+			engine.chat("Donations (in BTC) can be send to: 1BRoDCbnJ7kTS5dvVhjLdQnyqSWWjWC6SS");
+		}else if(tokens[1] == "lookup"){
+			if(typeof tokens[2] == "undefined"){
+				engine.chat("Usage: !cointrust lookup <username>");
+			}else{
+				$.ajax({
+					dataType: "json",
+					url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + tokens[2],
+					data1: data,
+					success: function (data1){
+						console.log(data1);
+						array = data1[0];
+						if(typeof array !== "undefined"){
+							engine.chat("Username: "+ array.uname +" | Suspicion Level: " + array.suspicion + " | Read the full profile at: " + array.link);
+						}else{	
+							engine.chat("User not found! (you can try to register it on cointrust.pw)");
+						}
+					}
+				});
 			}
-		});
+		}else{
+			if(typeof tokens[1] == "undefined"){
+				engine.chat("What do you want me to do Master?");
+			}else{
+				engine.chat("I do not understand your command. Please try again");
+			}
+			
+		}
 	}
 });
