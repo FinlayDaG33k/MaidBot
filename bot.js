00dc9eeb8d9b1313ae41d276222bd075b4b17450
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-07-16 12:08:06 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,36 @@
+// Check if jQuery is in the page, and if not, inject it in the page
+if(typeof jQuery === "undefined"){
+	// Yes, you really need jQuery for this script to work
+	var script = document.createElement('script'); 
+	script.src = 'https://code.jquery.com/jquery-3.0.0.min.js'; // the URL to the jQuery library
+	document.documentElement.firstChild.appendChild(script) // now append the script into HEAD, it will fetch and be executed
+}
+
+engine.on('msg', function(data) {
+    console.log('Chat message!...');
+	message = data.message;
+	if (message.indexOf("!cointrust") >= 0) {
+		tokens = message.split(" ", 2);
+		console.log(tokens);
+		console.log(tokens[1]);
+		$.ajax({
+			dataType: "json",
+			url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + tokens[1],
+			data1: data,
+			success: function (data1){
+				console.log(data1);
+				if(typeof data1[0].uname == 'undefined') {
+					console.log("User not found!");
+					engine.chat("User not found!");
+				}else{
+					console.log("Username: "+ data1[0].uname +" | Suspicion Level: " + data1[0].suspicion + " | Read the full profile at: " + data1[0].link);
+					engine.chat("Username: "+ data1[0].uname +" | Suspicion Level: " + data1[0].suspicion + " | Read the full profile at: " + data1[0].link);
+				}
+			}
+		});
+	}else{
+		console.log('Not a TrustyBot command, ignoring...');
+	}
+});
+
+	
