622efd4a039332de967e53af52dcd6ee45f172fe
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-07-18 11:13:07 +0200

    Update bot.js
    Fixed some issues :)

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-console.log("MaidBot v2016.07.17.15 Initialized");
+console.log("MaidBot v2016.07.18.11 Initialized");
 
 
 // Check if jQuery is in the page, and if not, inject it in the page
@@ -16,12 +16,14 @@ function display_lookup_user(suspicion,username,profilelink){
 		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to be pay his loans quite late. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "beggar"){
 		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to beg a lot. You can read his or her full profile at: " + profilelink);
+	}else if(suspicion == "hacker"){
+		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to be a hacker. Be careful with him or her Master. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "abuser"){
 		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to like abusing things. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "cleared"){
 		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to have a past of suspicion, but has cleared it. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "scammer"){
-		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to be a scammer! Please do not trust him Master! You can read his or her full profile at: " + profilelink);
+		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to be a scammer! Be careful with him or her Master. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "spammer"){
 		engine.chat("I was able to find "+ username +" on Cointrust, but he or she seems to spam a lot. You can read his or her full profile at: " + profilelink);
 	}else if(suspicion == "dwc"){
@@ -35,72 +37,78 @@ function display_lookup_user(suspicion,username,profilelink){
 engine.on('msg', function(data) {
 	message = data.message;
 	if(message.indexOf("!maidbot") == 0) {
-		tokens = message.split(" ");
-		if(typeof tokens[1] == "undefined"){
-			engine.chat("What do you want me to do Master?");
-		}else{
-			if(tokens[1].toLowerCase() == "help"){
-				engine.chat("You can view my wiki here: https://github.com/FinlayDaG33k/TrustyBot/wiki/Commands/");
-			}else if(tokens[1].toLowerCase() == "donate"){
-				engine.chat("You can send donations in BTC to: 1BRoDCbnJ7kTS5dvVhjLdQnyqSWWjWC6SS");
-			}else if(tokens[1].toLowerCase() == "lookup"){
-				if(typeof tokens[2] == "undefined"){
-					$.ajax({
+		if(data.channelName == "spam"){
+			tokens = message.split(" ");
+			if(typeof tokens[1] == "undefined"){
+				engine.chat("What do you want me to do Master?");
+			}else{
+				if(tokens[1].toLowerCase() == "help"){
+					engine.chat("You can view my wiki here: https://github.com/FinlayDaG33k/MaidBot/wiki/Commands/");
+				}else if(tokens[1].toLowerCase() == "donate"){
+					engine.chat("You can send donations in BTC to: 1BRoDCbnJ7kTS5dvVhjLdQnyqSWWjWC6SS");
+				}else if(tokens[1].toLowerCase() == "lookup"){
+					if(typeof tokens[2] == "undefined"){
+						$.ajax({
 						dataType: "json",
-						url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + data.username,
-						data1: data,
-						success: function (data1){
-							array = data1[0];
-							if(typeof array !== "undefined"){
-								display_lookup_user(array.suspicion,array.uname,array.link);
-							}else{	
-								engine.chat("I'm sorry, but I could not find the user you requested, but you can try to lookup the user here: https://www.cointrust.pw/?s=" + tokens[2]);
-							}
-						}
-					});
-				}else{
-					$.ajax({
-						dataType: "json",
-						url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + tokens[2],
-						data1: data,
-						success: function (data1){
-							array = data1[0];
-							display_lookup_user(array.suspicion,array.uname,array.link);
-						}
-					});
-				}
-			}else if(tokens[1].toLowerCase() == "worth"){
-				if(typeof tokens[2] !== "undefined"){
-					if(typeof tokens[3] !== "undefined"){
-						if(tokens[3].toLowerCase() == "eur"){
-							console.log(tokens[3]);
-							$.getJSON({
-								url: "https://blockchain.info/ticker",
-								array: data,
-								dataType: 'jsonp',
-								crossDomain: true,
-								success: function (array){
-									console.log(array);
-									engine.chat(tokens[2] + " BTC is worth " + (tokens[2] * array['EUR']['last']) + tokens[3].toUpperCase());						
+							url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + data.username,
+							data1: data,
+							success: function (data1){
+								array = data1[0];
+								if(typeof array !== "undefined"){
+									display_lookup_user(array.suspicion,array.uname,array.link);
+								}else{	
+									engine.chat("I'm sorry, but I could not find you on Cointrust, but you can try to lookup yourself here: https://www.cointrust.pw/?s=" + tokens[2]);
 								}
-							});
+							}
+						});
+					}else{
+						$.ajax({
+							dataType: "json",
+							url: "https://cointrust.pw/wp-json/wp/v2/profile?slug=" + tokens[2],
+							data1: data,
+							success: function (data1){
+								array = data1[0];
+								if(typeof array !== "undefined"){
+									display_lookup_user(array.suspicion,array.uname,array.link);
+								}else{	
+									engine.chat("I'm sorry, but I could not find the user you requested, but you can try to lookup the user here: https://www.cointrust.pw/?s=" + tokens[2]);
+								}
+							}
+						});
+					}
+				}else if(tokens[1].toLowerCase() == "worth"){
+					if(typeof tokens[2] !== "undefined"){
+						if(typeof tokens[3] !== "undefined"){
+							if(tokens[3].toLowerCase() == "eur"){
+								console.log(tokens[3]);
+								$.getJSON({
+									url: "https://blockchain.info/ticker",
+									array: data,
+									dataType: 'jsonp',
+									crossDomain: true,
+									success: function (array){
+										console.log(array);
+										engine.chat(tokens[2] + " BTC is worth " + (tokens[2] * array['EUR']['last']) + tokens[3].toUpperCase());						
+									}
+								});
+							}
+						}else{
+							engine.chat(tokens[2] + " Bits is worth " + (tokens[2] * 0.000001) + "BTC");
 						}
 					}else{
-						engine.chat(tokens[2] + " Bits is worth " + (tokens[2] * 0.000001) + "BTC");
+						engine.chat("I'm sorry, but I can not help you with this yet. Maybe you would like to run one of my other commands?");
 					}
 				}else{
-					engine.chat("I'm sorry, but I can not help you with this yet. Maybe you would like to run one of my other commands?");
+					engine.chat("I do not understand your command Master, please try again.");
 				}
-			}else{
-				engine.chat("I do not understand your command Master, please try again.");
 			}
-		}
-	}else if(message.indexOf("!rep") == 0) {
-		tokens = message.split(" ");
-		if(tokens[1].toLowerCase() == "maidbot" && tokens[2] == "+"){
-			engine.chat("I am glad to hear that you like my service Master.");
-		}else if(tokens[1].toLowerCase() == "maidbot" && tokens[2] == "-"){
-			engine.chat("I am sad to hear that you don't like my service Master");
+		}else if(message.indexOf("!rep") == 0) {
+			tokens = message.split(" ");
+			if(tokens[1].toLowerCase() == "maidbot" && tokens[2] == "+"){
+				engine.chat("I am glad to hear that you like my service Master.");
+			}else if(tokens[1].toLowerCase() == "maidbot" && tokens[2] == "-"){
+				engine.chat("I am sad to hear that you don't like my service Master");
+			}
 		}
 	}
 });
